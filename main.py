52f9b764002ab6827b7f86c3ad8cdda1d0129c28
Mohan Rajanna <44655155+mohanrajanna15@users.noreply.github.com>
2025-04-06 20:48:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,12 @@
 
 import helper
 import os
+import subprocess
 
 if __name__ == "__main__":
-    name = "Alice"
+    name = "Mohan"
     message = helper.greet(name)
     print(os.environ.get("PATH"))
+    result = subprocess.run("whoami", shell=True, capture_output=True, text=True)
+    print(result.stdout)
     print(message)
